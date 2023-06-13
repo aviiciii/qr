cc7170f517c85fe68e83cc88e470250cb2173c1e
aviiciii <laavesh1@gmail.com>
2023-06-13 14:37:36 +0530

    created qr gen

---
+++ script.js
@@ -0,0 +1,35 @@
+addEventListener("DOMContentLoaded", function() {
+    // get submit button
+    const submit = document.getElementById("submit");
+    // add event listener to submit button
+    submit.addEventListener("click", function() {
+        console.log("submit button clicked");
+        // get input value
+        const input = document.getElementById("input").value;
+        
+        console.log(input);
+
+        // add qr to output
+        const output = document.getElementById("output");
+
+        // clear output
+        output.innerHTML = "";
+
+        // create qr code
+        qr_src = `https://api.qrserver.com/v1/create-qr-code/?data=${input}&amp;size=100x100`
+
+        // create img element
+        const img = document.createElement("img");
+        img.src = qr_src;
+        img.alt = "QR Code";
+
+        // append img to output
+        output.appendChild(img);
+
+        // clear input
+        document.getElementById("input").value = "";
+
+
+    });
+
+});
