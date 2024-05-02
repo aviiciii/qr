6a68dcfdc4b8c101ff26eb693fb0f1df33cebcab
aviiciii <laavesh1@gmail.com>
2024-05-02 23:16:09 +0530

    structure created for pop-up

--- script.js
+++ script.js
@@ -26,10 +26,57 @@ addEventListener("DOMContentLoaded", function() {
         document.getElementById("qr-img").src = img_src;
 
         document.getElementById("output-div").style.display = "block";
+
+        var modal = document.getElementById("myModal");
+        var span = document.getElementsByClassName("close")[0];
+
+
+        // When the user clicks on the button, open the modal
+
+        modal.style.display = "block";
+
+    
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        };
+    
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        };
         
         
 
     });
+
+
+
+    // ---------------- QR CODE MODAL ----------------
+
+    // Get the modal, button and span
+    var generate_btn = document.getElementById("qr-btn");
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+    modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+    modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+    };
+
 
 
 
